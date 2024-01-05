982ca2e2e32283707cc13319438c22ccad59876d
Ryan Wong <ryanjameswong@outlook.com>
2024-01-05 11:41:08 -0500

    Handle number buttons input to stored numbers array

--- app.js
+++ app.js
@@ -1,18 +1,45 @@
 let firstNumber;
 let secondNumber;
 let operator;
-let displayValue;
+//let displayValue;
+let input = [];
 
 const display = document.querySelector('.display')
 const btnTest = document.getElementById('1');
+const btns = Array.from(document.querySelectorAll('.calc-button'));
 
-btnTest.addEventListener('click', () => {
-  console.log(btnTest);
-  display.textContent += '1';
-});
+function setButtonListener(btns){
+  for (let btn of btns){
+    btn.addEventListener('click', () => {
+      console.log(btn.value);
+      handleBtnInput(btn.value, btn.id)
+    });
+  }
+}
+
+function handleBtnInput(btnInput, btnID){
+  if (btnID === 'number'){
+    handleNumberInput(btnInput)
+    console.log("Handled: " + btnInput);
+  }
+}
+
+function handleNumberInput(btnInput) {
+  if (operator) {
+    secondNumber = secondNumber ? secondNumber + btnInput : btnInput;
+    input[2] = secondNumber;
+  } else {
+    firstNumber = firstNumber ? firstNumber + btnInput : btnInput;
+    input[0] = firstNumber;
+  }
+
+  console.log("input = " + input);
+  //updateDisplay()
+}
+
 
 function add(num1, num2){
-  return num1 + num2;
+  return +num1 + +num2 + '';
 }
 
 function subtract(num1, num2){
@@ -24,7 +51,7 @@ function multiply(num1, num2){
 }
 
 function divide(num1, num2){
-  if (num2 === 0) {
+  if (num2 === '0') {
     return "Error";
   }
   return num1 / num2;
@@ -42,3 +69,6 @@ function operate(num1, num2, operator){
   }
 }
 
+
+//On load
+setButtonListener(btns);
